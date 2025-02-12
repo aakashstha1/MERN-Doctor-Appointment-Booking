2b0d5f3812b3d0a514552a8cef2c44073f720b58
aakashstha1 <aakash.cerestha90@gmail.com>
2025-02-12 19:41:45 +0545

    twelvth commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster
+        toastOptions={{
+          style: {
+            background: "#333",
+            color: "#fff",
+            fontSize: "18px",
+          },
+        }}
+      />
     </BrowserRouter>
   </StrictMode>
 );
